bb9f9f007e10187ff1df440ae1b9903a235c6dbe
SoengSaravit <soengsaravit@gmail.com>
2017-07-30 10:34:23 +0700

    Initial Commit

---
+++ UserDefaults 30-07-2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserDefaults 30-07-2017
+//
+//  Created by Soeng Saravit on 7/30/17.
+//  Copyright © 2017 Soeng Saravit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
